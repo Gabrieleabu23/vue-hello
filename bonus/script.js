bf0b87128d22076b30d4d1c9c2f1c8c1d0553e0e
Gabrieleabu23 <kaidgabriele23@gmail.com>
2023-12-07 12:53:23 +0100

    add: bonus folder and added img

---
+++ bonus/script.js
@@ -0,0 +1,10 @@
+const {createApp }=Vue;
+
+createApp({
+    data(){
+        return{
+            messaggio:'Hello Vue',
+            immagine:'https://images.unsplash.com/photo-1632882765546-1ee75f53becb?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8aHRtbHxlbnwwfHwwfHx8MA%3D%3D'
+        }
+    }
+}).mount('#saluto');

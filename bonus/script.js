a3aaef40ece31079afc995dc28217306a5f82b32
Gabrieleabu23 <kaidgabriele23@gmail.com>
2023-12-07 12:56:45 +0100

    add: css class to vue for img

--- bonus/script.js
+++ bonus/script.js
@@ -4,7 +4,9 @@ createApp({
     data(){
         return{
             messaggio:'Hello Vue',
-            immagine:'https://images.unsplash.com/photo-1632882765546-1ee75f53becb?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8aHRtbHxlbnwwfHwwfHx8MA%3D%3D'
+            immagineClass:'immagine',
+            immagine:'https://images.unsplash.com/photo-1632882765546-1ee75f53becb?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8aHRtbHxlbnwwfHwwfHx8MA%3D%3D',
+            immagineAlt:'Immagine html gialla'
         }
     }
 }).mount('#saluto');
